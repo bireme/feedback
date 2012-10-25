bb455c9ce94e812e0c956e599c6cbad13b14ca16
moacirmoda <moa.moda@gmail.com>
2012-10-25 13:09:29 -0200

    adding signal to auto-create user profile

--- bireme/account/models.py
+++ bireme/account/models.py
@@ -13,3 +13,12 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     occupation = models.CharField(_("occupation"), max_length=255, blank=True, null=True)
     age = models.CharField(_("age"), max_length=255, blank=True, null=True, choices=AGE_CHOICES)
+
+def create_profile(sender, instance, created, **kwargs):
+    user = instance
+    try:
+        profile = user.get_profile()
+    except:
+        profile = UserProfile(user=user)
+        profile.save()
+models.signals.post_save.connect(create_profile, sender=User, dispatch_uid="some.unique.string.id")
