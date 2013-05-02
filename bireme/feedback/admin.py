b3ef0b7ce64283471742ac21b46c5aa582559edf
moacirmoda <moa.moda@gmail.com>
2013-05-02 17:00:05 -0300

    updating django admin

--- bireme/feedback/admin.py
+++ bireme/feedback/admin.py
@@ -1,14 +1,30 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
-from models import *
 from util.models import *
+from models import *
 
 class GenericAdmin(admin.ModelAdmin):
     
     exclude = ('updated', 'updater')
     list_display = ['__unicode__', 'creator', 'created']
     search_fields  = ['__unicode__',]
+
+class FeedbackAdmin(GenericAdmin):
+    model = Feedback
+    list_display = ['id', 'category', 'has_answer', 'is_error', 'application', 'version']
+    list_filter = ['is_error', 'application']
+    search_fields = ['id', 'problem']
+
+    def has_answer(self, obj):
+        if obj.answer:
+            return True
+        return False
+    has_answer.admin_order_field = 'answer'
+    has_answer.short_description = _("Has Answer?")
+    has_answer.boolean = True
+
     
-admin.site.register(Feedback, GenericAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(AditionalFeedback, GenericAdmin)
 admin.site.register(Category, GenericAdmin)
 admin.site.register(Country)
