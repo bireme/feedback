a1288d84051e4f892e71e052aa2f4dbd51ce20f2
moacirmoda <moa.moda@gmail.com>
2013-06-20 12:21:20 -0300

    adding problem field to search and showing category field in feedback form

--- bireme/feedback/admin.py
+++ bireme/feedback/admin.py
@@ -17,7 +17,7 @@ class FeedbackAdmin(GenericAdmin):
 
     fieldsets = (
         (_('Administrative'), {
-            'fields' : ['is_active', 'staff_comment', 'answer',],
+            'fields' : ['is_active', 'staff_comment', 'answer', 'category'],
         }), 
         (_('Information'), {
             'fields' : ['problem', 'blocker_error', 'application', 'version', 'ip',
@@ -28,7 +28,7 @@ class FeedbackAdmin(GenericAdmin):
 
     list_display = ['id', 'category', 'has_answer', 'is_error', 'application', 'version', 'creator']
     list_filter = ['is_error', 'application']
-    search_fields = ['id', 'problem']
+    search_fields = ['id', 'problem', 'answer', 'problem']
 
     def has_answer(self, obj):
         if obj.answer:
